48886cd3132ae31de06def753752b148bc07ac54
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-03 01:45:14 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,6 +3,10 @@ function paragraphSelector() {
   $('p');
 }
 
+$(document).ready(function paragraphSelector() {
+  // code to be executed goes here
+});
+
 function lastImageSelector() {
   $('div ul li img:last');
 }
