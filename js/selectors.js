ebedc34cf8e48e5cfc091d0e7edbe8024eb993dd
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-03 01:45:58 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,14 +1,4 @@
-// declare your functions here...
-
 $(document).ready(function paragraphSelector() {
-  // code to be executed goes here
   $('p');
 });
 
-function lastImageSelector() {
-  $('div ul li img:last');
-}
-
-function ninjaBabySelector() {
-  $('#baby-ninja');
-}
